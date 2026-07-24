d7c58d9e6626433b8a340bb70feaa134e5370c17
Prajwal-747 <prajwalsr28.9.08@gmail.com>
2026-07-24 09:12:26 +0530

    Finished Control Panel

--- src/main.py
+++ src/main.py
@@ -25,6 +25,17 @@ class QRToolkit(ctk.CTk):
         self.mainframe = ctk.CTkFrame(
             self
         )
+        self.mainframe.grid_rowconfigure(0, weight =1)
+        self.mainframe.grid_columnconfigure(0, weight=1)
+        self.mainframe.grid_columnconfigure(1, weight=2)
+        self.controlPanel = ctk.CTkFrame(
+            self.mainframe,
+            corner_radius=10
+        )
+        self.previewPanel = ctk.CTkFrame(
+            self.mainframe,
+            corner_radius=10
+        )
         self.statusbar = ctk.CTkFrame(
             self,
             height=30,
@@ -35,6 +46,135 @@ class QRToolkit(ctk.CTk):
         self.topbar.grid(row=0,column=0,sticky="ew")
         self.mainframe.grid(row=1,column=0,sticky="nsew")
         self.statusbar.grid(row=2,column=0,sticky="ew")
+        self.controlPanel.grid(
+            row=0,
+            column=0,
+            padx=(15,8),
+            pady = 15,
+            sticky="nsew"
+        )
+        self.previewPanel.grid(
+            row=0,
+            column=1,
+            padx=(8,15),
+            pady=8,
+            sticky="nsew"
+        )
+        self.controlPanel.grid_columnconfigure(0, weight=1)
+        self.generateLabel=ctk.CTkLabel(
+            self.controlPanel,
+            text="Generate QR",
+            font=("Segoe UI", 22, "bold")
+        )
+        self.generateLabel.grid(
+            row=0,
+            column=0,
+            padx=20,
+            pady=(20,10),
+            sticky="w"
+        )
+
+        self.dataLabel=ctk.CTkLabel(
+            self.controlPanel,
+            text="Data"
+        )
+        self.dataLabel.grid(
+            row=2,
+            column=0,
+            padx=20,
+            sticky="w"
+        )
+
+        self.dataEntry = ctk.CTkEntry(
+            self.controlPanel,
+            placeholder_text="Enter Text or URL..."
+        )
+
+        self.dataEntry.grid(
+            row=3,
+            column=0,
+            padx=20,
+            pady=(5,15),
+            sticky="ew"
+        )
+
+        self.fileLabel=ctk.CTkLabel(
+            self.controlPanel,
+            text="Filename"
+        )
+        self.fileLabel.grid(
+            row=4,
+            column=0,
+            padx=20,
+            sticky="w"
+        )
+
+        self.fileEntry = ctk.CTkEntry(
+            self.controlPanel,
+            placeholder_text="qr_code.png"
+        )
+
+        self.fileEntry.grid(
+            row=5,
+            column=0,
+            padx=20,
+            pady=(5,15),
+            sticky="ew"
+        )
+
+        self.foregroundlabel = ctk.CTkLabel(
+            self.controlPanel,
+            text="Foreground Color"
+        )
+        self.foregroundlabel.grid(
+            row=6,
+            column=0,
+            padx=20,
+            sticky="w"
+        )
+        self.foregroundMenu = ctk.CTkOptionMenu(
+            self.controlPanel,
+            values=[
+                "Black",
+                "Blue",
+                "Red",
+                "Green",
+                "Purple"
+            ]
+        )
+        self.foregroundMenu.grid(
+            row=7,
+            column=0,
+            padx=20,
+            pady=(5,15),
+            sticky="ew"
+        )
+        self.backgroundlabel = ctk.CTkLabel(
+            self.controlPanel,
+            text="Background Color"
+        )
+        self.backgroundlabel.grid(
+            row=8,
+            column=0,
+            padx=20,
+            sticky="w"
+        )
+        self.backgroundMenu = ctk.CTkOptionMenu(
+            self.controlPanel,
+            values=[
+                "White",
+                "Black",
+                "Gray",
+                "Yellow"
+            ]
+        )
+        self.backgroundMenu.grid(
+            row=9,
+            column=0,
+            padx=20,
+            pady=(5,15),
+            sticky="ew"
+        )
 
         self.topbar.grid_columnconfigure(0, weight=1)
         self.titleLabel = ctk.CTkLabel(
@@ -49,6 +189,7 @@ class QRToolkit(ctk.CTk):
             pady=15,
             sticky="w"
         )
+
         self.aboutButton = ctk.CTkButton(
             self.topbar,
             text="About",
